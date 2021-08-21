650bb376b1cb3da80437c3eeb53a4bc35fb7061a
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2021-08-21 14:21:03 +0100

    learning Blazor

---
+++ BlazorServerCrud/Data/EmployeeService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerCrud.Data
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private List<Employee> employees = new List<Employee>
+    {
+        new Employee
+        {
+            Id = Guid.NewGuid(),
+            Name = "Olabamiji"
+        },
+         new Employee
+        {
+            Id = Guid.NewGuid(),
+            Name = "Paul"
+        }
+    };
+
+        public void AddEmployee(Employee employee)
+        {
+            var id = Guid.NewGuid();
+            employee.Id = id;
+            employees.Add(employee);
+        }
+
+        public void DeleteEmployee(Guid id)
+        {
+            var employee = GetEmployee(id);
+            employees.Remove(employee);
+        }
+
+        public Employee GetEmployee(Guid id)
+        {
+            return employees.SingleOrDefault(x => x.Id == id);
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+
+        public void updateemployee(Employee employee)
+        {
+            var getEmployee = GetEmployee(employee.Id);
+            getEmployee.Name = employee.Name;
+        }
+    }
+}
---
+++ BlazorServerCrud/Data/IEmployeeService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerCrud.Data
+{
+
+    interface IEmployeeService
+    {
+
+        List<Employee> GetEmployees();
+
+        Employee GetEmployee(Guid id);
+
+        void updateemployee(Employee employee);
+        void AddEmployee(Employee employee);
+
+        void DeleteEmployee(Guid id);
+    }
+}
